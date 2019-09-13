56ebfe5d8067db7819b32e7f27ecfd585cffd061
Tiffany Nguyen <tiffany.phuongn@gmail.com>
2019-09-12 20:11:31 -0700

    tried implementing getNextToken

--- lexer.h
+++ lexer.h
@@ -1,10 +1,38 @@
 #include <iostream>
 #include <string>
+#include <cstdlib>
+#include <fstream>
 #include "token.h"
 using namespace std;
 
 class Lexer{
     public:
     Token getNextToken();
-    private:
 };
+
+
+Token Lexer::getNextToken(){
+    /*
+    Was thinking first, scan the file in here
+    Get all the tokens and print them out when file is done scanned
+    */
+   ifstream in_stream;
+   in_stream.open("test.txt");
+   if(!in_stream){
+       cout << "Cannot open file." << endl;
+       exit(1);
+   }
+   char c;
+   while(in_stream.get(c)){
+       if(isspace(c)){
+           //something with creating a new token
+       }
+       else if(c == '.'){
+           //something here to deal with the float
+       }
+       else if(c == '{' || c == '}' || c ==';' || c== '<' || c == '>'){
+           return Token(c);
+       }
+       //what about finding keywords?
+   }
+}
