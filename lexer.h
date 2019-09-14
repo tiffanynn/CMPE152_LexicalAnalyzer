9dc2e73a838c818f4a7830a1983b6cc9b714b00e
Tiffany Nguyen <tiffany.phuongn@gmail.com>
2019-09-13 19:17:36 -0700

    i don't think i know what i'm doing

--- lexer.h
+++ lexer.h
@@ -47,14 +47,57 @@ vector<Token> Lexer::getNextToken(){
   }
 
 //need to consider keywords and stuff
+string temp = ""; //need this to store in letters 
 
 for(int i = 0; i < size; i++){
-    if(c == '{' || c == '}' || c ==';' || c== '<' || c == '>'){
-           string symbol(1, c);
+    //checking if there's letters first
+    if(isalpha(file_contents[i])){
+        //probably going to hard code everything
+        temp += file_contents[i];
+        //don't know how to deal with any letters oop
+
+        // if(file_contents[i] == 't'){
+        //     if(file_contents[i] == 't'){}
+        //     else{}
+        // }
+
+    }
+
+    //considering the symbols
+    else if(file_contents[i] == '{' || file_contents[i] == '}' || file_contents[i] ==';' || file_contents[i] = '+' ||
+    file_contents[i] == '-' ||file_contents[i] == '*' || file_contents[i] == '(' || file_contents[i] == ')' || file_contents[i] == '/'){
+           string symbol(1, file_contents[i]);
            Token something(symbol, symbol);
            obtained_tokens.push_back(something);
-        
        }
+    else if(file_contents[i]  == '>' || file_contents[i] == '<')
+    {
+        temp = file_contents[i];
+        if(file_contents[i]  == '>'){
+            if(file_contents[i+1] == '='){
+                temp += file_contents[i+1];
+                Token something(temp, "GE");
+                obtained_tokens.push_back(something);
+            }
+            else{
+          string symbol(1, file_contents[i]);
+           Token something(symbol, symbol);
+           obtained_tokens.push_back(something);
+           }
+        }
+        else if(file_contents[i]  == '<'){
+            if(file_contents[i+1] == '='){
+                temp += file_contents[i+1];
+                Token something(temp, "LE");
+                obtained_tokens.push_back(something);
+            }
+else{
+          string symbol(1, file_contents[i]);
+           Token something(symbol, symbol);
+           obtained_tokens.push_back(something);
+           }
+        }
+    }
    else if( i == (size-1)){
            Token end("EOF", "EOF");
            obtained_tokens.push_back(end);
