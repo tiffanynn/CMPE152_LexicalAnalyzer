4deacba6f676bc7a403cdd73ba5d1b93b355af8f
Tiffany Nguyen <tiffany.phuongn@gmail.com>
2019-09-13 21:31:26 -0700

    got the symbols part to work

--- lexer.h
+++ lexer.h
@@ -6,114 +6,177 @@
 #include "token.h"
 using namespace std;
 
-class Lexer{
+class Lexer
+{
     public:
-    Lexer();
-    vector<Token> obtained_tokens;
-    vector<Token> getNextToken();
-    string token_names[18] = {"AND",
-"BASE_TYPE","BREAK","DO", "ELSE","EQ","FALSE","FOR", "GE",
-"ID", "IF", "LE","NE","NUM","OR", "REAL", "TRUE", "WHILE"
-};
-    void print();
+        Lexer();
+        vector<Token> obtained_tokens;
+        vector<Token> getNextToken();
+        string token_names[18] =
+        { "AND", "BASE_TYPE", "BREAK", "DO", "ELSE", "EQ", "FALSE", "FOR", "GE",
+                "ID", "IF", "LE", "NE", "NUM", "OR", "REAL", "TRUE", "WHILE" };
+        void print();
 };
 
+Lexer::Lexer(){
+    obtained_tokens.push_back(Token("0","0"));
+}
 
-//is this a function that we can use 
-vector<Token> Lexer::getNextToken(){
+//is this a function that we can use
+vector<Token> Lexer::getNextToken()
+{
     /*
-    Was thinking first, scan the file in here
-    Get all the tokens and print them out when file is done scanned
-    */
+     Was thinking first, scan the file in here
+     Get all the tokens and print them out when file is done scanned
+     */
 
-   ifstream in_stream;
-   in_stream.open("test.txt");
-   if(!in_stream){
-       cout << "Cannot open file." << endl;
-       exit(1);
-   }
+    ifstream in_stream;
+    in_stream.open("test.txt");
+    if (!in_stream)
+    {
+        cout << "Cannot open file." << endl;
+        exit(1);
+    }
 
-   char c;
-   char file_contents[100];
-   int size = 0;
-   /*
-   1. Scan characters from the file into an array or something lol
-   2. Have a for-loop comparing the characters and tokens
-   3. Push into the vector
-   */ 
-  while(in_stream.get(c)){
-      file_contents[size] = c;
-      size++;
-  }
+    char c;
+    char file_contents[100];
+    int size = 0;
+    /*
+     1. Scan characters from the file into an array or something lol
+     2. Have a for-loop comparing the characters and tokens
+     3. Push into the vector
+     */
+    while (in_stream.get(c))
+    {
+        file_contents[size] = c;
+        size++;
+    }
 
 //need to consider keywords and stuff
-string temp = ""; //need this to store in letters 
+    string temp = ""; //need this to store in letters
 
-for(int i = 0; i < size; i++){
-
-    
-    //checking if there's letters first
-    if(isalpha(file_contents[i])){
-        //probably going to hard code everything
-        temp += file_contents[i];
-        //don't know how to deal with any letters oop
-
-        // if(file_contents[i] == 't'){
-        //     if(file_contents[i] == 't'){}
-        //     else{}
-        // }
-
-    }
-
-    //considering the symbols
-    else if(file_contents[i] == '{' || file_contents[i] == '}' || file_contents[i] ==';' || file_contents[i] = '+' ||
-    file_contents[i] == '-' ||file_contents[i] == '*' || file_contents[i] == '(' || file_contents[i] == ')' || file_contents[i] == '/'){
-           string symbol(1, file_contents[i]);
-           Token something(symbol, symbol);
-           obtained_tokens.push_back(something);
-       }
-    else if(file_contents[i]  == '>' || file_contents[i] == '<')
+    for (int i = 0; i < size; i++)
     {
-        temp = file_contents[i];
-        if(file_contents[i]  == '>'){
-            if(file_contents[i+1] == '='){
-                temp += file_contents[i+1];
-                Token something(temp, "GE");
-                obtained_tokens.push_back(something);
-            }
-            else{
-          string symbol(1, file_contents[i]);
-           Token something(symbol, symbol);
-           obtained_tokens.push_back(something);
-           }
+
+        //checking if there's letters first
+        if (isalpha(file_contents[i]))
+        {
+            //probably going to hard code everything
+            temp += file_contents[i];
+            //don't know how to deal with any letters oop
+
+            // if(file_contents[i] == 't'){
+            //     if(file_contents[i] == 't'){}
+            //     else{}
+            // }
+
         }
-        else if(file_contents[i]  == '<'){
-            if(file_contents[i+1] == '='){
-                temp += file_contents[i+1];
-                Token something(temp, "LE");
-                obtained_tokens.push_back(something);
+
+        //considering the symbols
+        else if (file_contents[i] == '{')
+        {
+            string symbol(1, file_contents[i]);
+            Token something(symbol, symbol);
+            obtained_tokens.push_back(something);
+        }
+        else if (file_contents[i] == '}')
+        {
+            string symbol(1, file_contents[i]);
+            Token something(symbol, symbol);
+            obtained_tokens.push_back(something);
+        }
+        else if (file_contents[i] == ';')
+        {
+            string symbol(1, file_contents[i]);
+            Token something(symbol, symbol);
+            obtained_tokens.push_back(something);
+        }
+        else if (file_contents[i] == '+')
+        {
+            string symbol(1, file_contents[i]);
+            Token something(symbol, symbol);
+            obtained_tokens.push_back(something);
+        }
+        else if (file_contents[i] == '-')
+        {
+            string symbol(1, file_contents[i]);
+            Token something(symbol, symbol);
+            obtained_tokens.push_back(something);
+        }
+        else if (file_contents[i] == '*')
+        {
+            string symbol(1, file_contents[i]);
+            Token something(symbol, symbol);
+            obtained_tokens.push_back(something);
+        }
+        else if (file_contents[i] == '(')
+        {
+            string symbol(1, file_contents[i]);
+            Token something(symbol, symbol);
+            obtained_tokens.push_back(something);
+        }
+        else if (file_contents[i] == ')')
+        {
+            string symbol(1, file_contents[i]);
+            Token something(symbol, symbol);
+            obtained_tokens.push_back(something);
+        }
+        else if (file_contents[i] == '/')
+        {
+            string symbol(1, file_contents[i]);
+            Token something(symbol, symbol);
+            obtained_tokens.push_back(something);
+        }
+        else if (file_contents[i] == '>' || file_contents[i] == '<')
+        {
+            temp = file_contents[i];
+            if (file_contents[i] == '>')
+            {
+                if (file_contents[i + 1] == '=')
+                {
+                    temp += file_contents[i + 1];
+                    Token something(temp, "GE");
+                    obtained_tokens.push_back(something);
+                }
+                else
+                {
+                    string symbol(1, file_contents[i]);
+                    Token something(symbol, symbol);
+                    obtained_tokens.push_back(something);
+                }
             }
-else{
-          string symbol(1, file_contents[i]);
-           Token something(symbol, symbol);
-           obtained_tokens.push_back(something);
-           }
+            else if (file_contents[i] == '<')
+            {
+                if (file_contents[i + 1] == '=')
+                {
+                    temp += file_contents[i + 1];
+                    Token something(temp, "LE");
+                    obtained_tokens.push_back(something);
+                }
+                else
+                {
+                    string symbol(1, file_contents[i]);
+                    Token something(symbol, symbol);
+                    obtained_tokens.push_back(something);
+                }
+            }
+        }
+        else if (i == (size - 1))
+        {
+            Token end("EOF", "EOF");
+            obtained_tokens.push_back(end);
         }
     }
-   else if( i == (size-1)){
-           Token end("EOF", "EOF");
-           obtained_tokens.push_back(end);
-    }
+
+    return obtained_tokens;
 }
 
-
-           
-      
-   return obtained_tokens;
-}
-
-void Lexer::print(){
-    for(int i =0; i < obtained_tokens.size; i++){
-        cout << obtained_tokens[i].lexerme << '\t' << obtained_tokens[i].token_value << endl;
+void Lexer::print()
+{
+    unsigned int size = obtained_tokens.size();
+    for (int i = 0; i < size; i++)
+    {
+        cout << obtained_tokens[i].lexerme << '\t'
+                << obtained_tokens[i].token_value << endl;
     }
 }
